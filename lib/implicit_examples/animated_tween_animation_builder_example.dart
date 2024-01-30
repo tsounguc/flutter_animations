6ac77d7d2d4d33cadf31334416a40832adbf29d9
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-30 09:54:10 -0500

    AnimatedTWeenAnimations code

--- lib/implicit_examples/animated_tween_animation_builder_example.dart
+++ lib/implicit_examples/animated_tween_animation_builder_example.dart
@@ -11,20 +11,31 @@ class PulsatingCircleAnimation extends StatelessWidget {
         title: const Text('Pulsating Circle Animation'),
       ),
       body: Center(
-        child: Container(
-          width: size,
-          height: size,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.blue,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.blue.withOpacity(0.5),
-                blurRadius: size,
-                spreadRadius: size / 2,
+        child: TweenAnimationBuilder(
+          duration: const Duration(milliseconds: 1500),
+          tween: Tween<double>(begin: 0, end: 200),
+          builder: (BuildContext context, size, Widget? widget) {
+            return Container(
+              width: size,
+              height: size,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.pink.withOpacity(0.5),
+                    blurRadius: size,
+                    spreadRadius: size / 2,
+                  ),
+                  BoxShadow(
+                    color: Colors.blue.withOpacity(0.5),
+                    blurRadius: size,
+                    spreadRadius: size / 2,
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
