83d8402713b8a3c52db2cd5570ae435855a72e98
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-30 11:14:58 -0500

    Logo animation

--- lib/explicit_examples/login_animation.dart
+++ lib/explicit_examples/login_animation.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
-class LoginScreenAnimation extends StatelessWidget {
+class LoginScreenAnimation extends StatefulWidget {
   const LoginScreenAnimation({super.key});
+
+  @override
+  State<LoginScreenAnimation> createState() => _LoginScreenAnimationState();
+}
+
+class _LoginScreenAnimationState extends State<LoginScreenAnimation>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> logoFaceAnimation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1500));
+    logoFaceAnimation = Tween<double>(begin: 0, end: 1).animate(controller);
+    controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +28,11 @@ class LoginScreenAnimation extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const FlutterLogo(
-              size: 100.0,
+            FadeTransition(
+              opacity: logoFaceAnimation,
+              child: const FlutterLogo(
+                size: 100.0,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:animation_starter_code/implicit_examples/animated_color_pallete.dart';
-import 'package:animation_starter_code/implicit_examples/animated_shopping_cart_button.dart';
-import 'package:animation_starter_code/implicit_examples/animated_tween_animation_builder_example.dart';
+import 'package:animation_starter_code/explicit_examples/login_animation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const PulsatingCircleAnimation(),
+      home: const LoginScreenAnimation(),
     );
   }
 }
